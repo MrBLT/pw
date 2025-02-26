baf6313785d312f0f6464f689188d5b9524c8e7e
Chris Weaver <chris.weaver@infor.com>
2025-02-26 10:26:05 -0500

    no message

--- recordings/tests/csi-version.spec.ts
+++ recordings/tests/csi-version.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://mingle-portal.inforcloudsuite.com/v2/SLSGDENA209_AX1/5e29a9ce-6356-4b64-a59b-207c7fb1b5c7');
   await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('ElevateD1RF_00');
+  await page.getByRole('textbox', { name: 'Username' }).fill('some_username');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('SUNConf@dminD1RF');
+  await page.getByRole('textbox', { name: 'Password' }).fill('some_password');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.locator('iframe[name="syteline_44_5e29a9ce-6356-4b64-a59b-207c7fb1b5c7"]').contentFrame().locator('#configCombo-trigger-picker').click();
   await page.locator('iframe[name="syteline_44_5e29a9ce-6356-4b64-a59b-207c7fb1b5c7"]').contentFrame().getByRole('option', { name: 'SLSGDENA209_AX1_DALS' }).click();
